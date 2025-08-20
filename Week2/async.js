2d84b13ae2451f7aef07d9a5ffa1a930c84d0d56
kaifyyy <mkaifm728@gmail.com>
2025-08-20 22:16:45 +0530

    Changes Commit

--- Week2/async.js
+++ Week2/async.js
@@ -1,1 +1,18 @@
- 
+ // this is for async.js
+
+ // normal function in Js
+
+ function sum(a, b){
+    return a+b;
+ }
+
+ let ans = sum(2, 3)
+ console.log(ans);
+
+// find sum of 1 to number
+// 1 => n
+
+
+function sum(n){
+
+}
