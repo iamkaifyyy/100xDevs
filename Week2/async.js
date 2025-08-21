7f57692c5a1b29528ad1e0fe3991595501b5cca0
kaifyyy <mkaifm728@gmail.com>
2025-08-21 18:39:56 +0530

    Initial Commit

--- Week2/async.js
+++ Week2/async.js
@@ -139,7 +139,10 @@ fs.readFile("a.txt", "utf-8", print);
 
 fs.readFile("b.txt", "utf-8", print);
 
-console.log("Done!");
+
+function readFile(filePath, encoding, op){
+    op("error", "hi there")
+}
 
 
 
