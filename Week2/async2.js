093bb0d4c83c869f5a895d395a158d46e3f5d21f
kaifyyy <mkaifm728@gmail.com>
2025-08-25 19:12:27 +0530

    commit

--- Week2/async2.js
+++ Week2/async2.js
@@ -42,7 +42,8 @@ setTimeout(function(){
 
 // promisified way of writing setTimeout 
 
-// promise chaining--
+// promise way of doing it
+
  
 function setTimeoutPromisified(duration){
     return new Promise(function(resolve){
@@ -61,4 +62,46 @@ setTimeoutPromisified(1000).then(function(){
 });
 
 
-console.log("outside of callback hell")
+console.log("outside of callback hell");
+
+ 
+
+//  NOW THE WAY OF PROMISE CHAINING--
+
+setTimeoutPromisified(1000)
+.then(function(){
+    console.log("hi");
+    return setTimeoutPromisified(3000);
+})
+.then(function(){
+    console.log("hello");
+    return setTimeoutPromisified(5000);
+})
+.then(function(){
+    console.log("hello there")
+})
+
+
+
+
+// async await code for promise chaning 
+
+function setTimeoutPromisified(duration){
+    return new Promise(function(resolve){
+        setTimeout(resolve, duration);
+    });
+}
+
+
+async function solve(){
+    await setTimeoutPromisified(1000);
+    console.log("hi");
+    await setTimeoutPromisified(3000);
+    console.log("hello");
+    await setTimeoutPromisified(5000);
+    console.log("hi there");
+}
+
+solve();
+
+console.log("after solve function")
