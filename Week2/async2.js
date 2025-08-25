7db4587c6bd8a1fe1f2a1beca78bfdbf8eb827f5
kaifyyy <mkaifm728@gmail.com>
2025-08-25 18:17:08 +0530

    commit changes

---
+++ Week2/async2.js
@@ -0,0 +1,43 @@
+// this async class is from Cohort 3/Week2-offlinevideos/5.Callback hell,rejects and async/await
+
+
+
+// creating a promiified version of setTimeout
+
+function setTimeoutPromisified(){
+    return new Promise(function(resolve){
+        setTimeout(resolve, duration)
+    })
+}
+
+function callback(){
+    console.log("1 second has passed!")
+}
+
+// setTimeout => promisified setTimeout
+
+setTimeoutPromisified(1000).then(callback)
+
+
+
+
+// callback hell example
+
+function callback(){
+    console.log("hi");
+}
+
+// callback example---
+
+setTimeout(function(){
+    console.log("hi");
+    setTimeout(function(){
+        console.log("hello");
+        setTimeout(function(){
+            console.log("hello there");
+        }, 5000);
+    }, 3000)
+}, 1000);
+
+
+// 
