00d82371f66e1de4c951ff60b190520de3b65b3c
kaifyyy <mkaifm728@gmail.com>
2025-08-28 01:40:53 +0530

    Commit Changes

---
+++ Week 4/http server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+function calculateSum(){
+    let ans = 0;
+    for (let i = 0; i <= n; i++) {
+        ans = ans + i;
+    }
+    return ans;
+}
+
+
+
+const app = express;
+app.length("/", function(req, res){
+    const n = req.query
+})
+
+
+
