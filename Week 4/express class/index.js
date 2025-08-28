cc62ce46315471e96e67c4628fcafb6f6cc3ad26
kaifyyy <mkaifm728@gmail.com>
2025-08-28 17:59:34 +0530

    Commits

--- Week 4/express class/index.js
+++ Week 4/express class/index.js
@@ -6,12 +6,20 @@ const express = require("express");
 
 const app = express();
 
-
+ function sum(n){
+    let ans = 0;
+    for(let i = 1; i<=n ; i++){
+        ans = ans + i;
+    }
+    return ans;
+ }
 
 app.get("/", function(req, res){
-     res.send("hi there")
-})
+    const n = req.query.n;
+    const ans = sum(n)
+    res.send(`the ans is` + ans);
 
+})
 
 app.listen(3000);
 
