9835f0d3fa7882ead112009cb7c9d618b071a71e
kaifyyy <mkaifm728@gmail.com>
2025-08-30 14:46:45 +0530

    commit

--- Week 4/express class/index.js
+++ Week 4/express class/index.js
@@ -3,16 +3,17 @@
 const express = require("express");
 const app = express();
 
-console.log(app);
-
-app.get("/", function(req, res){
-    res.send();
-})
-
-
-
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
+})
+
+app.use((req, res) => {
+    console.log(req);
+    console.log("request Recieved!");
+
+    res.send("this is a basic response!");
+
+    
 })
