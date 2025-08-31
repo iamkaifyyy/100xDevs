624d9399744ea2358d2447978901c8cc6a9f8879
kaifyyy <mkaifm728@gmail.com>
2025-08-31 17:06:27 +0530

    changesCommit

--- Week 4/Backend - Node.js & express/script.js
+++ Week 4/Backend - Node.js & express/script.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express();
 
+
+// construction of middleware
+
+app.use(function(req, res, next){
+
+    
+});
+
+
+
 app.get("/", function(req, res){
     res.send('hello world');
 })
