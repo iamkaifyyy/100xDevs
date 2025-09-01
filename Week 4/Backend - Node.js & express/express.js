b6d199857b988951932e5b5cf285430b267c46f8
kaifyyy <mkaifm728@gmail.com>
2025-09-02 00:27:45 +0530

    commit

---
+++ Week 4/Backend - Node.js & express/express.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+
+
+// construction of middleware
+
+app.use(function(req, res, next){
+    console.log(req); 
+
+    next();
+});
+
+
+
+  
+// app.get is used to create Routes
+ 
+
+app.get("/", function(req, res){
+    res.send('hello world');
+})
+
+
+app.get("/home", function(req, res){
+    res.send('Home Page is this');
+})
+
+  
+app.listen(3000);
--- Week 4/Backend - Node.js & express/script.js
+++ Week 4/Backend - Node.js & express/script.js
@@ -1,29 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-
-// construction of middleware
-
-app.use(function(req, res, next){
-    console.log(req); 
-
-    next();
+app.get("/", function(req, res){
+    res.send('Hello world');
 });
 
-
-
-  
-// app.get is used to create Routes
- 
-
-app.get("/", function(req, res){
-    res.send('hello world');
-})
-
-
-app.get("/home", function(req, res){
-    res.send('Home Page is this');
-})
-
-  
 app.listen(3000);
