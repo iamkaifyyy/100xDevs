bf80a36da16c149979591d22f8ab731555795e18
kaifyyy <mkaifm728@gmail.com>
2025-09-09 12:50:00 +0530

    commits

--- Week 4/http servers/index.js
+++ Week 4/http servers/index.js
@@ -9,11 +9,14 @@ app.get("/asd", function(req, res){
     res.send("this is asd route");
 })
 
+app.get("/routes", function(req, res){
+    res.send("kaifyyyy");
+})
+
+
+
 
 app.listen(3000);
 
 
 
-
-r0
- 
