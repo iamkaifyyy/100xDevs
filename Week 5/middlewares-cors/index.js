d8d37c4f70c4a6668d4a1bee1ae7ddf669ec9321
kaifyyy <mkaifm728@gmail.com>
2025-10-01 01:14:36 +0530

    req Count

--- Week 5/middlewares-cors/index.js
+++ Week 5/middlewares-cors/index.js
@@ -4,7 +4,15 @@ const express = require("express");
 
 const app = express();
 
+let requestCount = 0;
+
 app.get("/sum", function(req, res){
+
+    requestCount = requestCount + 1;
+    console.log("total no of requests = " + requestCount);
+
+    console.log("total no of requests = " + requestCount)
+
     const a = req.query.a;
     const b = req.query.b;
 
