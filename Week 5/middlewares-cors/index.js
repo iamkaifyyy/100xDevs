84dd18ec390027878693ea8a4914c8dd03664d57
kaifyyy <mkaifm728@gmail.com>
2025-10-01 01:12:07 +0530

    commit

--- Week 5/middlewares-cors/index.js
+++ Week 5/middlewares-cors/index.js
@@ -4,4 +4,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/sum", function(req, res){
+    const a = req.query.a;
+    const b = req.query.b;
 
+    res.json({
+        ans: a + b
+    });
+});
+
+
+app.get("/multiply", function(req, res){
+    const a = req.query.a;
+    const b = req.query.b;
+
+    res.json({
+        ans: a + b
+    });
+});
+
+app.listen(3000); 
