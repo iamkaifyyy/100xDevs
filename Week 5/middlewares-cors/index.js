bb6740fca67fb20df2dafe7ed84557e7937bf6ae
kaifyyy <mkaifm728@gmail.com>
2025-10-07 02:52:43 +0530

    commit

--- Week 5/middlewares-cors/index.js
+++ Week 5/middlewares-cors/index.js
@@ -1,2 +1,16 @@
 const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/sum', (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  
+  res.json({
+    ans: a + b
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
