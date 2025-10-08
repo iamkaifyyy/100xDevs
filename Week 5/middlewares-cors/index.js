3d21644d54aa274575e0b19023638fcd75e7561c
kaifyyy <mkaifm728@gmail.com>
2025-10-09 00:47:43 +0530

    commit

--- Week 5/middlewares-cors/index.js
+++ Week 5/middlewares-cors/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+let requestCount = 0;
 
 app.get('/sum', (req, res) => {
+  requestCount = requestCount + 1;
+  console.log("total no of requests = " + requestCount);
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
   
@@ -10,5 +13,8 @@ app.get('/sum', (req, res) => {
     ans: a + b
   })
 })
+
+
+app.get("")
 
 app.listen(3000)
