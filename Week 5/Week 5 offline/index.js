935b7182af3adfedccc7a70e8f06663f1d75a6cf
kaifyyy <mkaifm728@gmail.com>
2025-10-19 20:06:59 +0530

    Changes

---
+++ Week 5/Week 5 offline/index.js
@@ -0,0 +1,17 @@
+// map, filter, arrow functions
+
+function sum(a, b){
+    return a + b;
+
+}
+
+const sum = (a, b) =>{
+    return a + b;
+}
+
+app.get("/", (req, res){
+    
+})
+
+const ans = sum(1, 2);
+console.log(ans);
