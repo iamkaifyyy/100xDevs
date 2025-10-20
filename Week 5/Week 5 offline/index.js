e5de192b8e45a51ecde60a77b646e39d5234347a
kaifyyy <mkaifm728@gmail.com>
2025-10-20 14:03:03 +0530

    commit

--- Week 5/Week 5 offline/index.js
+++ Week 5/Week 5 offline/index.js
@@ -1,15 +1,2 @@
-// map, filter, arrow functions
+// filter functions
 
-const input = [1, 2, 3, 4, 5];
-
-// other Solution using map 
-
-function transform(){
-    return i * 2;
-}
-
-const ans = input.map(transform);
-console.log(ans);
-
-
- 
