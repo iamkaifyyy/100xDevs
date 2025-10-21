847b64baad585f905e357fb4a4ec7cb2005d69b5
kaifyyy <mkaifm728@gmail.com>
2025-10-21 11:30:04 +0530

    commit

--- Week 5/Week 5 offline/axios-fetch/index.js
+++ Week 5/Week 5 offline/axios-fetch/index.js
@@ -60,4 +60,14 @@ getData();
 
 // using Axios as with Async-Await Syntax
 
-import axios form 
+import axios from "axios"
+
+async function getData(){
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1")
+        console.log(response.data);
+    } catch(error) {
+        console.error("Error", error)
+    }
+}
+
