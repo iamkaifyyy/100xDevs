2e99b77225dd6b2e8dae6187658dc8f64bb2246a
kaifyyy <mkaifm728@gmail.com>
2025-10-21 18:23:37 +0530

    commit

--- Week 6/HTTP DeepDive/index.js
+++ Week 6/HTTP DeepDive/index.js
@@ -1,3 +1,34 @@
 // this video is for http deep dive - Authentication 
 
-c
+const express = require("express")
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function generateToken(){
+    return Math.random()
+}
+
+app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username, 
+        password: password
+    })
+
+    res.json({
+        message:"You are signed in"
+    })
+
+})
+
+app.post("/signin", function(req, res){
+   
+})
+
+app.listen(3000); // tht the http server is listening on port 3000
+
