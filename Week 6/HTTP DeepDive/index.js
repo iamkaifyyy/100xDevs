718bb5a75caeb2834f3aaa910a31c15a30a00d9e
kaifyyy <mkaifm728@gmail.com>
2025-10-25 13:15:53 +0530

    commit

--- Week 6/HTTP DeepDive/index.js
+++ Week 6/HTTP DeepDive/index.js
@@ -1,32 +1,46 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 app.use("express.json");
 
 const users = [];
 
-app.post("/signup", function(req, res){
-    // input Validation
-    const username = req.body.username;
-    const password = req.body.password;
+function generateToken() {
+  let options = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ];
 
-    
+  let token = "";
 
-    users.push({
-        username: username,
-        password: password
-    });
+  for (let i = 0; i < 32; i++) {
+    // use a simple function here
+    token = token + options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
 
-    res.json({
-        message: "Youre Signed in"
-    })
-})
+app.post("/signup", function (req, res) {
+  // input Validation
+  const username = req.body.username;
+  const password = req.body.password;
 
+  users.push({
+    username: username,
+    password: password,
+  });
 
-
-app.post("/signin", function(req, res){
-    
+  res.json({
+    message: "Youre Signed in",
+  });
 });
 
-app.listen(3000);E
+app.post("/signin", function (req, res) {});
 
+app.listen(3000);
+E;
