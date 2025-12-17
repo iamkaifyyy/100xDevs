ed432d7ff53f191a84dfeae64c51c2f566aeac59
kaifyyy <mkaifm728@gmail.com>
2025-12-18 00:59:03 +0530

    commit

---
+++ week-7/mongoDB-7-1/mongoDB/script.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.get("/signup", (req, res) => {
+
+})
+
+app.get("/signup", (req, res) => {
+
+})
+
+app.listen(3000); 
